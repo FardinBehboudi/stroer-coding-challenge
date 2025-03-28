6b1425052dedb3ebe3413683ac0654afa30c9193
Fardin Behboudi <f_behboudi@hotmail.com>
2025-03-28 11:15:35 +0100

    Initial project setup

---
+++ src/main/java/com/fardin/stroer/StroerCodingChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.fardin.stroer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StroerCodingChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StroerCodingChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fardin/stroer/StroerCodingChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fardin.stroer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StroerCodingChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
